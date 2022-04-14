7d4f5a321bfd8a856b516dfe4f8bf0535dee1eaa
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-04-13 18:56:18 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media');
+
+// logs mongoose queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
