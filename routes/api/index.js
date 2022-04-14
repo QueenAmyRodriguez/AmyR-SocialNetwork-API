12590c4bfd7bb41511179ae207212b4630d79f7d
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-04-13 21:44:41 -0700

    added thought routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const thoughtsRoutes = require('./thoughts-routes');
 
 // add prefix of /users to routes 
 router.use('/users', userRoutes);
+router.use('/thoughts', thoughtsRoutes);
 
 module.exports = router;
--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,1 +1,18 @@
 const router = require('express').Router();
+const {
+    getAllThoughts,
+    addThoughts
+} = require('../../controllers/thoughts-controller')
+
+// GET all thoughts and POST to create thoughts
+
+router
+.route('/')
+.get(getAllThoughts)
+
+// create thoughts associated with user
+router.route('/:userId').post(addThoughts);
+
+
+
+module.exports = router;
