8b9047309513c370c34f90f0cee968861ccb9a6a
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-04-13 19:21:02 -0700

    user model created

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    // thoughts: [
+    //     {
+    //         type: Schema.Types.ObjectId,
+    //         ref: "Thoughts"
+    //     }
+    // ],
+    // friends: [
+    //     {
+    //         types: Schema.Types.ObjectId,
+    //         ref: 'User'
+    //     }
+    // ]
+});
+
+// create User model using UserSchema
+const User = model('User', UserSchema);
+
+
+module.exports = User;
