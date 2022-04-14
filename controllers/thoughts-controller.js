9f45378a0eb74528350f492c879c0da329142383
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-04-13 21:24:07 -0700

    added thoughts controller

--- controllers/thoughts-controller.js
+++ controllers/thoughts-controller.js
@@ -1,2 +1,39 @@
+const { Thoughts, User } = require('../models');
 
+const thoughtsController = {
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thoughts.find({})
+            .then(dbThoughtsData => res.json(dbThoughtsData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    // add thoughts to user model
+    addThoughts({ params, body }, res) {
+        Thoughts.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
 
+    // remove thoughts {
+    // removeThoughts() {
+
+    // }
+};
+
+module.exports = thoughtsController;
